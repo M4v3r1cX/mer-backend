3b0bd81065fa2f4ee22ee2ec2f5e690ea97d79ab
Maverick Vlasilev <m4v3r1cx.thanatos@gmail.com>
2023-08-05 19:31:30 -0400

    Creacion usuario service

---
+++ mer-backend-services/src/main/java/com/bsodsoftware/merbackend/services/UsuarioService.java
@@ -0,0 +1,18 @@
+package com.bsodsoftware.merbackend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bsodsoftware.merbackend.jpa.entities.Usuario;
+import com.bsodsoftware.merbackend.jpa.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	public Usuario save(Usuario entity) {
+		return usuarioRepository.save(entity);
+	}
+}
