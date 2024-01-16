2c42a95baa6f11bea21b0258e67452fdae49ada1
Maverick Vlasilev <m4v3r1cx.thanatos@gmail.com>
2024-01-15 22:27:27 -0300

    Corrección en filtro de OAs

--- src/main/java/com/bsodsoftware/merbackend/jpa/repository/ObjetivoAprendizajeHijoRepository.java
+++ src/main/java/com/bsodsoftware/merbackend/jpa/repository/ObjetivoAprendizajeHijoRepository.java
@@ -12,7 +12,8 @@ public interface ObjetivoAprendizajeHijoRepository extends JpaRepository<Objetiv
 	@Query(value = "select oah.* from objetivo_aprendizaje_hijo oah "
 			+ "inner join objetivo_aprendizaje oa on oah.id_objetivo_aprendizaje = oa.id "
 			+ "inner join oa_hijo_subcategoria ohr on oah.id = ohr.id_oa_hijo "
-			+ "inner join subcategoria_red r on ohr.id_subcategoria = r.id "
+			+ "inner join subcategoria_red sr on ohr.id_subcategoria = sr.id "
+			+ "inner join red r on sr.id_red = r.id "
 			+ "inner join oa_nivel oan on oa.id = oan.id_oa "
 			+ "inner join nivel n on oan.id_nivel = n.id "
 			+ "where n.id = :idNivel and r.id = :idRed", nativeQuery = true)
